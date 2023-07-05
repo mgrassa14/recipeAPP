25b3a21bc1078b40e364dabbcd4867208c175058
Maria Grassa <mgrassa00gmail.com>
2023-07-05 15:07:38 -0700

    added a details page

--- main_app/urls.py
+++ main_app/urls.py
@@ -9,6 +9,6 @@ urlpatterns = [
     path('about/', views.about, name='about'),
     # localhost:8000/recipes
     path('recipes/', views.recipes_index, name='index'),
-    
-    # path('recipes/<int:recipe_id>/', views.recipes_detail, name='detail'),
+    #
+    path('recipes/<int:recipe_id>/', views.recipes_detail, name='detail'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -30,6 +30,6 @@ def recipes_index(request):
     recipes = Recipe.objects.all()
     return render(request, 'recipes/index.html', { 'recipes': recipes })
 
-# def recipes_detail(request, recipe_id):
-#     recipe = Recipe.objects.get(id=recipe_id)
-#     return render(request, 'recipes/detail.html', { 'recipe': recipe })
+def recipes_detail(request, recipe_id):
+    recipe = Recipe.objects.get(id=recipe_id)
+    return render(request, 'recipes/detail.html', { 'recipe': recipe })
